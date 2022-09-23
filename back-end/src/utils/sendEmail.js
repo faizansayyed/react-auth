95e99bcad17f404bc173c4edbc04406d0cec193b
Faizan Sayyed <faizansayyed36@gmail.com>
2022-09-23 20:04:15 +0530

    removed logs

--- back-end/src/utils/sendEmail.js
+++ back-end/src/utils/sendEmail.js
@@ -5,7 +5,6 @@ dotenv.config();
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const sendEmail = ({ to, from, subject, text, html }) => {
-    console.log({ to, from, subject, text, html })
     const msg = { to, from, subject, text, html }
 
     return sendgrid.send(msg)
